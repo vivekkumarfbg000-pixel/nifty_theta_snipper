8f51037b15b736e64b3fdbd16c588b80e7562f3d
Vivek Kumar <vivekkumarfbg000@gmail.com>
2026-04-10 10:55:57 +0530

    chore: make run_bot.bat generic for VPS

--- run_bot.bat
+++ run_bot.bat
@@ -1,8 +1,8 @@
 @echo off
 cd /d "%~dp0"
-set PY_PATH=C:\Users\vivek\AppData\Local\Python\pythoncore-3.14-64\python.exe
+set PY_PATH=python
 echo Starting Nifty Theta Sniper Dashboard...
 start /b %PY_PATH% dashboard_api.py
 echo Starting Trading Scheduler...
-%PY_PATH% daily_scheduler.py
+%PY_PATH% vps_scheduler.py
 pause
